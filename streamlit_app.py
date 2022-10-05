5920ca370bece8e1c74c71e9d3d8953161b973ef
arkmtech22 <113525666+arkmtech22@users.noreply.github.com>
2022-10-05 20:30:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 
-# Welcome to Streamlit!
+"""# Welcome to Streamlit!
 Edit `/streamlit_app.py` to customize this app to your heart's 
 
 desire :heart:
@@ -16,7 +16,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just 
 
 a few lines of code:
-
+"""
 
 
 with st.echo(code_location='below'):
