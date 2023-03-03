dc97256d97ac0e952e3fc6d10ceeed8695a328e7
arkmtech22 <113525666+arkmtech22@users.noreply.github.com>
2023-03-03 22:28:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -216,3 +216,23 @@ if submit:
             st.write('Congratulation',name,'You are not diabetic')
         else:
             st.write(name," we are really sorry to say but it seems like you are Diabetic.")
+            
+            
+            
+            
+import streamlit as st
+from multiapp import MultiApp
+from apps import quant, discrete, normal, proportions, studentt, allttests, chisquare, linearregression, anova # import your app modules here
+st.set_page_config(layout="wide")
+app = MultiApp()
+
+# Add all your application here
+app.add_app("Quantitative Stats", quant.app)
+app.add_app("Discrete Probability", discrete.app)
+app.add_app("Normal Probability", normal.app)
+app.add_app("Proportions", proportions.app)
+app.add_app("Student's T", studentt.app)
+app.add_app("All t-Tests", allttests.app)
+app.add_app("Chi-Square", chisquare.app)
+app.add_app("Linear Regression", linearregression.app)
+app.add_app("ANOVA", anova.app)
