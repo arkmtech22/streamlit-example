bb157111691358cb59e5367ad738eeeaa0f390a2
arkmtech22 <113525666+arkmtech22@users.noreply.github.com>
2023-11-30 21:57:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,38 +6,6 @@ import pandas as pd
 # Suppress the deprecation warning about np.bool
 # warnings.filterwarnings("ignore", category=FutureWarning, module="numpy")
 
-
-# Create a Streamlit app
-st.title("Pima Diabetes Prediction App")
-
-# Sidebar with sliders for user input
-st.sidebar.header("Enter Patient Information")
-
-pregnancies = st.sidebar.slider("Pregnancies", 0, 17, 3)
-glucose = st.sidebar.slider("Glucose", 0, 199, 117)
-blood_pressure = st.sidebar.slider("Blood Pressure", 0, 122, 72)
-skin_thickness = st.sidebar.slider("Skin Thickness", 0, 99, 23)
-insulin = st.sidebar.slider("Insulin", 0, 846, 30)
-bmi = st.sidebar.slider("BMI", 0.0, 67.1, 32.0)
-dpf = st.sidebar.slider("Diabetes Pedigree Function", 0.0, 2.42, 0.3725)
-age = st.sidebar.slider("Age", 21, 81, 29)
-
-# Create a dictionary for user input
-user_input = {
-    "Pregnancies": [pregnancies],
-    "Glucose": [glucose],
-    "BloodPressure": [blood_pressure],
-    "SkinThickness": [skin_thickness],
-    "Insulin": [insulin],
-    "BMI": [bmi],
-    "DiabetesPedigreeFunction": [dpf],
-    "Age": [age]
-}
-
-# Display user input data
-st.sidebar.subheader("User Input Data:")
-st.sidebar.write(user_input)
-
 # Upload the Pima Indian Diabetes dataset
 st.sidebar.header("Upload Pima Indian Diabetes Dataset")
 uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type=["csv"])
@@ -73,3 +41,37 @@ if st.button("Previous Page", key="previous"):
     page = max(1, page - 1)
 if st.button("Next Page", key="next"):
     page = min((len(df) - 1) // rows_to_display + 1, page + 1)
+
+
+# Create a Streamlit app
+st.title("Pima Diabetes Prediction App")
+
+# Sidebar with sliders for user input
+st.sidebar.header("Enter Patient Information")
+
+pregnancies = st.sidebar.slider("Pregnancies", 0, 17, 3)
+glucose = st.sidebar.slider("Glucose", 0, 199, 117)
+blood_pressure = st.sidebar.slider("Blood Pressure", 0, 122, 72)
+skin_thickness = st.sidebar.slider("Skin Thickness", 0, 99, 23)
+insulin = st.sidebar.slider("Insulin", 0, 846, 30)
+bmi = st.sidebar.slider("BMI", 0.0, 67.1, 32.0)
+dpf = st.sidebar.slider("Diabetes Pedigree Function", 0.0, 2.42, 0.3725)
+age = st.sidebar.slider("Age", 21, 81, 29)
+
+# Create a dictionary for user input
+user_input = {
+    "Pregnancies": [pregnancies],
+    "Glucose": [glucose],
+    "BloodPressure": [blood_pressure],
+    "SkinThickness": [skin_thickness],
+    "Insulin": [insulin],
+    "BMI": [bmi],
+    "DiabetesPedigreeFunction": [dpf],
+    "Age": [age]
+}
+
+# Display user input data
+st.sidebar.subheader("User Input Data:")
+st.sidebar.write(user_input)
+
+
